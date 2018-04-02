7a5841cd4fb2084b3137c3dd4885c70a25e59d12
Lee Jun Hee <>
2018-04-02 11:48:34 +0900

    - chang normal class to data class

--- app/src/main/java/com/example/junheelee/imagepickerexample/ImageData.kt
+++ app/src/main/java/com/example/junheelee/imagepickerexample/ImageData.kt
@@ -5,6 +5,4 @@ import android.net.Uri
 /**
  * Created by junhee.lee on 2018. 3. 16..
  */
-public class ImageData(val file: Uri, val path: String) {
-
-}
+data class ImageData(val file: Uri, val path: String)
